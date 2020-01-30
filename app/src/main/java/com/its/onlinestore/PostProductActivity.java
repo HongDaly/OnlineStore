ad3cd2204549f537ff8026c7bfb2471a918377b7
Blockdev <daly.fanzo@gmail.com>
2020-01-30 16:26:06 +0700

    chip

--- app/src/main/java/com/its/onlinestore/PostProductActivity.java
+++ app/src/main/java/com/its/onlinestore/PostProductActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ClipData;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -12,30 +15,48 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.its.onlinestore.helper.Constant;
+import com.its.onlinestore.helper.FirebaseHelper;
 import com.its.onlinestore.helper.PermissionControllerHelper;
+import com.its.onlinestore.model.Category;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PostProductActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private RecyclerView rcImageGallery;
     private ImageView ivImagePicker;
+    private ImageView[] imageViews = new ImageView[4];
+    private LinearLayout llChooseCategory;
+    private ChipGroup cgCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_product);
 
-        rcImageGallery = findViewById(R.id.app_rv_image_gallery);
         ivImagePicker = findViewById(R.id.app_iv_pick_image);
+        imageViews[0] = findViewById(R.id.app_image_1);
+        imageViews[1] = findViewById(R.id.app_image_2);
+        imageViews[2] = findViewById(R.id.app_image_3);
+        imageViews[3] = findViewById(R.id.app_image_4);
+        cgCategories = findViewById(R.id.app_cg_categories);
 
+
+        llChooseCategory = findViewById(R.id.app_ll_choose_category);
 
 
         ivImagePicker.setOnClickListener(this);
+        llChooseCategory.setOnClickListener(this);
     }
 
     @Override
@@ -51,6 +72,9 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
             }else{
                 Toast.makeText(getApplicationContext(),"Permission Denail",Toast.LENGTH_LONG).show();
             }
+        }else if(id == llChooseCategory.getId()){
+//            Choose category
+            chooseCategory();
         }
     }
 
@@ -73,19 +97,27 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
 
             if(data != null){
                 ClipData clipData = data.getClipData();
-                ArrayList<Bitmap> bitmaps = new ArrayList<>();
                 if(clipData != null){
                     Log.d("err","image : "+clipData.getItemCount());
-                    for(int i = 0;i<clipData.getItemCount();i++){
-                        try {
-                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),clipData.getItemAt(i).getUri());
-                            bitmaps.add(bitmap);
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                    if(clipData.getItemCount() <= 4){
+                        for(int i = 0;i<clipData.getItemCount();i++){
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),clipData.getItemAt(i).getUri());
+                                imageViews[i].setImageBitmap(bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }else{
+                        for(int i = 0;i<4;i++){
+                            try {
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),clipData.getItemAt(i).getUri());
+                                imageViews[i].setImageBitmap(bitmap);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
-
-                    ivImagePicker.setImageBitmap(bitmaps.get(0));
                 }else {
                     Log.d("err","err");
                 }
@@ -94,6 +126,87 @@ public class PostProductActivity extends AppCompatActivity implements View.OnCli
                 Log.d("err","err");
                 Toast.makeText(getApplicationContext(),"0",Toast.LENGTH_LONG).show();
             }
+        }
+    }
+
+
+
+
+
+//    Category Choose Dialog;
+
+    private void chooseCategory(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+//        get Categories
+        FirebaseHelper.getCategories().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if(queryDocumentSnapshots != null){
+                    ArrayList<String> categories = new ArrayList<>();
+                    for(DocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                        Category category = documentSnapshot.toObject(Category.class);
+                        if(category != null){
+                            categories.addAll(category.getSub_category());
+                        }
+                    }
+
+                    if(categories.size() != 0){
+                        final String[] categoriesChooser = new String[categories.size()];
+                        final boolean[] checkedCategory = new boolean[categories.size()];
+                        for(int i = 0 ; i < categories.size();i++){
+                            categoriesChooser[i] = categories.get(i);
+                            checkedCategory[i] = false;
+                        }
+
+
+                        builder.setMultiChoiceItems(categoriesChooser, checkedCategory, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                                checkedCategory[which] = isChecked;
+                            }
+                        });
+
+
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Do something when click positive button
+//                              selected items display in chip group
+                                ArrayList<String> chooseCategories = new ArrayList<>();
+                                for (int i = 0 ; i<categoriesChooser.length;i++){
+                                    if(checkedCategory[i]){
+                                        chooseCategories.add(categoriesChooser[i]);
+                                    }
+                                }
+                                dialog.dismiss();
+                                initCategoryToChipGroup(chooseCategories);
+                            }
+                        });
+
+                        // Set the neutral/cancel button click listener
+                        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Do something when click the neutral button
+                                dialog.dismiss();
+                            }
+                        });
+
+                        AlertDialog dialog = builder.create();
+                        // Display the alert dialog on interface
+                        dialog.show();
+
+                    }
+                }
+            }
+        });
+    }
+    private void initCategoryToChipGroup(ArrayList<String> categories){
+        for (String cate : categories){
+            Chip chip = new Chip(this);
+            chip.setText(cate);
+            cgCategories.addView(chip);
         }
     }
 }
